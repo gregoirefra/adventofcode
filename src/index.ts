20b5f25a52317f1ddc59f57f25c624c58c0c5fdd
Francis Grégoire <francis.gregoire@dormakaba.com>
2020-12-04 23:11:05 -0500

    Add day 4;

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import { day01part1, day01part2 } from './2020/01';
 import { day02part1, day02part2 } from './2020/02';
 import { day03part1, day03part2 } from './2020/03';
+import { day04part1, day04part2 } from './2020/04';
 
 const run = async () => {
   console.log('day 1 part 1 : ' + (await day01part1()));
@@ -10,5 +11,7 @@ const run = async () => {
   console.log('day 2 part 2 : ' + (await day02part2()));
   console.log('day 3 part 1 : ' + (await day03part1()));
   console.log('day 3 part 2 : ' + (await day03part2()));
+  console.log('day 4 part 1 : ' + (await day04part1()));
+  console.log('day 4 part 2 : ' + (await day04part2()));
 };
 run();
